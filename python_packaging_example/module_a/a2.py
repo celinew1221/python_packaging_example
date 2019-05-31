1be8f6ad21ec9f74c4e61af8ae5a153afc9bf627
Celine Wei <c.wei@surgicalsafety.com>
2019-05-31 16:17:52 -0400

    add function in current folder

--- python_packaging_example/module_a/a2.py
+++ python_packaging_example/module_a/a2.py
@@ -1,2 +1,5 @@
+from . import a1
+
 def a2(array):
+    a1([0])
     print(array)
--- python_packaging_example/print.py
+++ python_packaging_example/print.py
@@ -1,4 +1,4 @@
-# relative path only works for packaging but if you call this within the package it won't work
+# relative path only work for packaging but if you call this within the package it won't work
 # so if you want to call main, and print to test this package, try
 # python3 -m python_packaging_example.main
 # python3 -m python_packaging_example.print
